1e54fb316b64bde4339c7a0b4244878bd0261089
unknown <chenjh1@digitalchina.com>
2019-08-26 10:57:47 +0800

    修改

--- src/views/Echarts2.vue
+++ src/views/Echarts2.vue
@@ -17,15 +17,23 @@ export default {
         initWhater() {
     　　　　let myChart = this.$echarts.init(document.getElementById('myChart'))
     　　　　myChart.setOption({
-                tooltip: {
+                tooltip: { //提示框设置
                     trigger: 'item',
+                    backgroundColor: 'rgba(255,255,255,1)',
+                    textStyle:{
+                        color:'#000'
+                    },
                     formatter: "{a} <br/>{b}: {c} ({d}%)"
                 },
                 legend: {
                     orient: 'horizontal', //  vertical
                     x: 'left',
                     y:'bottom',
-                    data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+                    data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎'],
+                    icon: "circle", //circle，rect ，roundRect，triangle，diamond，pin，arrow，none
+                    itemWidth: 10,  // 设置宽度
+                    itemHeight: 10, // 设置高度
+                    // itemGap: 100 // 设置间距
                 },
                 series: [
                     {
@@ -33,6 +41,12 @@ export default {
                         type:'pie',
                         radius: ['30%', '50%'],
                         avoidLabelOverlap: false,
+                        itemStyle:{ //设置饼图板块之间的间距
+                            normal: {
+                                borderWidth: 1, 
+                                borderColor: '#fff',
+                            }
+                        },
                         label: {
                             normal: {
                                 show: false,
